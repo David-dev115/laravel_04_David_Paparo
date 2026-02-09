45a0eca9b613f919c91636276299cd8fc5a47ead
David Paparo <d.paparo@tiscali.it>
2026-02-09 17:04:43 +0100

    avanzamento esercizio 2

--- resources/views/chi_siamo.blade.php
+++ resources/views/chi_siamo.blade.php
@@ -1,20 +1,20 @@
 <x-layout>
 
 
-    <div class="container-fluid+ bg-secondary min-vh-100 d-flex align-items-center">
+    <div class="container-fluid bg-secondary min-vh-100 d-flex align-items-center">
         <div class="row w-100 justify-content-center">
-            <h2>Chi siamo</h2>
+            <h2 class="text-center">Chi siamo</h2>
             @foreach ($chi_siamo as $ele)
                 <div class="col-12 col-md-3 mb-4 d-flex">
                     <div class="card h-100 shadow-sm flex-fill">
                 {{-- riferimento al componente --}}
-                        <team-card
+                        <x-team-card
                         name="{{$ele['name']}}"
                         surname="{{$ele['surname']}}"
                         role="{{$ele['role']}}"
                         >
 
-                        </team-card>
+                        </x-team-card>
                     </div>
                 </div>
             @endforeach
--- resources/views/components/team-card.blade.php
+++ resources/views/components/team-card.blade.php
@@ -1,12 +1,16 @@
 
-
+{{-- chiarire questo @props --}}
+@props(['name', 'surname', 'role'])
 
 
 <div class="card-body text-center">
     <h5 class="card-title">{{ $name}} {{ $surname }}</h5>
-    <h6 class="card-subtitle mb-2 text-muted">{{ $role }}</h6>
+    <h6 class="card-subtitle mb-2 text-muted">{{$role}}</h6>
     <p class="card-text">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab sunt quos id dolores
         provident.
     </p>
 </div>
+
+
+
